fa26996c39198f2559ace38740a00d94b869c0dc
Harry Mumford-Turner <harrymt@me.com>
2017-10-01 16:09:59 +0100

    Added basic CSS and additional flickr fields

--- src/app/flickr-feed.component.ts
+++ src/app/flickr-feed.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
 import { FeedService } from './feed.service';
 import { FormControl } from '@angular/forms';
-import {Observable} from 'rxjs/Observable'
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'flickr-feed',
   template: `
-    <div>
-      <h2>Flickr Feed</h2>
-      <input type="text" [formControl]="tagSearch">
-      <ul>
-        <li *ngFor="let item of items | async">{{item.title}}</li>
-      </ul>
+    <div class='flickr-feed'>
+      <h2>Flickr Public Feed</h2>
+      <input class="search-box" value="harry" type="text" [formControl]="tagSearch">
+      <div class='card-list'>
+        <div class='card' *ngFor="let item of items | async">
+          <div class='card-image'>
+            <img src='{{ item.media.m }}'>
+          </div>
+          <div class='card-text'>
+            <div class='row'>
+              <h2>{{item.title}}</h2>
+            </div>
+            <div class='row'>
+              <a href='{{ item.author_id }}'>{{ item.author }}</a>
+              <span>{{ item.published }}</span>
+              <a href='{{ item.link }}'>View on Flickr</a>
+            </div>
+            <div class='row'>
+              {{ item.tags }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  `
+  `,
+  styleUrls: ['./flickr-feed.css']
 })
 
 export class FlickrFeedComponent {
