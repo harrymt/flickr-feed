7a63ef6752845f7c808e61c64bf45a867eecccb3
Harry Mumford-Turner <harrym@gwsmedia.com>
2017-10-05 13:42:08 +0100

    Separated the API into variable

--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -6,13 +6,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class FeedService {
 
+  private api_url = 'https://api.flickr.com/services/feeds/photos_public.gne?tagmode=all&jsoncallback=JSONP_CALLBACK&format=json';
+
   constructor (
     private jsonp: Jsonp
   ) {}
 
   getTagFeed(tag: string) {
     return this.jsonp
-      .get('https://api.flickr.com/services/feeds/photos_public.gne?tagmode=all&jsoncallback=JSONP_CALLBACK&format=json&tags=' + tag)
+      .get(this.api_url + '&tags=' + tag)
       .map((response) => response.json()).map(val => {
           return val.items;
       });
